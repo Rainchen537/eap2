2be79ab33fde2e87bbe542aaf388a683741790d6
雨晨 <66208218+Rainchen537@users.noreply.github.com>
2025-09-10 07:17:57 +0800

    name

--- database/init/01-init.sql
+++ database/init/01-init.sql
@@ -1,8 +1,8 @@
 -- 创建数据库（如果不存在）
-CREATE DATABASE IF NOT EXISTS quizonly CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS aiagent CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- 使用数据库
-USE quizonly;
+USE aiagent;
 
 -- 设置时区
 SET time_zone = '+08:00';
